fa0d7d1611dd26142b0b5c5b6d7f5272cbb16305
xarminux <71028441+xarminux@users.noreply.github.com>
2023-04-17 20:17:38 +0300

    Add files via upload
    created navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar.css'
+import {ROUTES} from '../routes/routes'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/" className="navbar-brand">AutoMinus</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/add-listing" className="nav-link">Įdėti skelbimą</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Visi skelbimai</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">Apie mus</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
