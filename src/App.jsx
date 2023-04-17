0cd350a40cc1405a76e72edb898d55fc34aae203
xarminux <71028441+xarminux@users.noreply.github.com>
2023-04-17 20:20:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Main from './components/Main'
+import {Route, Routes} from "react-router-dom";
+import Navbar from './components/navbar/navbar'
+import HomePage from './pages/Home/HomePage'
+import {ROUTES} from './components/routes/routes'
+import AddListing from './components/addListing/AddListing';
 
 const App = () => {
   return (
     <>
-        <Header/>
-        <Main />
+      <Navbar/>
+      <Routes>
+        <Route path={ROUTES.HOME} element = {<HomePage />}></Route>
+        <Route path={ROUTES.ADD_LISTING} element = {< AddListing/>}></Route>
+      </Routes>
     </>
   )
 }
