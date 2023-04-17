eebbe81c9571cefb5b824176bf5cb19fbd666e70
xarminux <71028441+xarminux@users.noreply.github.com>
2023-04-17 21:48:18 +0300

    Add files via upload

---
+++ src/pages/CarList/CarListPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CarList from '../../components/cars/CarList'
+
+
+
+const CarListPage= () => {
+  return (
+    <div>
+    <CarList/>
+    </div>
+    
+  )
+}
+
+export default CarListPage;
