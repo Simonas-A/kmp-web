1d4e9589fdfb942dcac5a913c50bfbff0f4ebc46
xarminux <71028441+xarminux@users.noreply.github.com>
2023-04-17 21:47:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/navbar/navbar'
 import HomePage from './pages/Home/HomePage'
 import {ROUTES} from './components/routes/routes'
 import AddListing from './components/addListing/AddListing';
+import CarListPage from './pages/CarList/CarListPage';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path={ROUTES.HOME} element = {<HomePage />}></Route>
         <Route path={ROUTES.ADD_LISTING} element = {< AddListing/>}></Route>
+        <Route path={ROUTES.CAR_LIST} element = {< CarListPage/>}></Route>
       </Routes>
     </>
   )
